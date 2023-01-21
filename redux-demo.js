896779e0201f7b4903a0e51a24a3f4d0cfe4e947
onomlight <pass041@nate.com>
2023-01-21 15:54:56 +0900

    리덕스 완성 후 실행 오류발생

--- redux-demo.js
+++ redux-demo.js
@@ -8,12 +8,26 @@ const counterReducer = (state, action) => {
 
   return {
     // 새로운 객체를 리턴 -> 이 객체는 어떠한 구조가 가능하며 개발자 재량으로 변함
-    counter: 0,
+    // counter: 0,
     // 카운터가 0으로 생성된 객체를 리턴
+    counter: state.counter + 1,
+    // 자동으로 받는 state를 참조하여 counter 값을 엑세스 하고 새로된 상태를 리턴함
+    // 이러면 리듀서가 완성됨
   }
 }
 
-const store = redux.createStore()
+const store = redux.createStore(counterReducer)
 // 데이터를관리하는게 리듀서함수에 결정됨
 
 // === 리덕스 라이브러리에서 받아서 기존상태에서 받음
+
+// 먼저 구독 시작  그러기위해서 새상수를 넣을것
+
+const counterSubscriber = () => {
+  const latestSate = store.getState()
+  console.log(latestSate)
+}
+
+store.subscribe(counterSubscriber)
+
+// 이후 node redux-demo.js  라고 터미널에 치면 코드 실행됨 >> 카운터에 오류 발생함
