d1a042695749e7d6fa2cd9bd26138d6819ed7cb4
onomlight <pass041@nate.com>
2023-01-22 00:51:34 +0900

    감소하는 함수 대입하여 작동원리 확인

--- redux-demo.js
+++ redux-demo.js
@@ -6,14 +6,23 @@ const redux = require('redux')
 const counterReducer = (state = { counter: 0 }, action) => {
   // 해당상태에서 리듀서함수에 액션값을 받을것 => 궁극적으로 리덕스 라이브러리에 의해 실행
 
-  return {
-    // 새로운 객체를 리턴 -> 이 객체는 어떠한 구조가 가능하며 개발자 재량으로 변함
-    // counter: 0,
-    // 카운터가 0으로 생성된 객체를 리턴
-    counter: state.counter + 1,
-    // 자동으로 받는 state를 참조하여 counter 값을 엑세스 하고 새로된 상태를 리턴함
-    // 이러면 리듀서가 완성됨
+  //@ 일반적으로 리럭스를 사용할때 리듀서 내부에서 다른 일을 하는게 목표 !! // 액션 변수를 활용함
+  if (action.type === 'increment') {
+    return {
+      // 새로운 객체를 리턴 -> 이 객체는 어떠한 구조가 가능하며 개발자 재량으로 변함
+      // counter: 0,
+      // 카운터가 0으로 생성된 객체를 리턴
+      counter: state.counter + 1,
+      // 자동으로 받는 state를 참조하여 counter 값을 엑세스 하고 새로된 상태를 리턴함
+      // 이러면 리듀서가 완성됨
+    }
   }
+  if (action.type === 'decrement') {
+    return {
+      counter: state.counter - 1,
+    }
+  }
+  return state
 }
 
 const store = redux.createStore(counterReducer)
@@ -38,3 +47,7 @@ store.subscribe(counterSubscriber)
 
 store.dispatch({ type: 'increment' }) // dispatch는 액션을 발송하는 메소드
 // 카운터를 증가시키기 위해 ‘increment’라는 타입 추가
+
+store.dispatch({ type: 'decrement' })
+// 감소 타입
+//>> 카운터가 증가했다 감소하는것으로 출력됨
