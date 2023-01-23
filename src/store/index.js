253f5d98b83b1a057a1be3609456b8538dc919bc
onomlight <pass041@nate.com>
2023-01-23 15:19:24 +0900

    스토어 만들기

---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+//여기서는 리덕스 로직을 저장할것
+import { createStore } from 'redux'
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === 'increment') {
+    return {
+      counter: state.counter + 1,
+    }
+  }
+  if (action.type === 'decrement') {
+    return {
+      counter: state.counter - 1,
+    }
+  }
+  return state
+}
+const store = createStore(counterReducer)
+
+export default store
