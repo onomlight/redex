0869ea6dc4662c906b842149b8c34d7540660780
onomlight <pass041@nate.com>
2023-01-24 17:54:00 +0900

    5 단위 증가 함수 넣기

--- src/store/index.js
+++ src/store/index.js
@@ -6,6 +6,16 @@ const counterReducer = (state = { counter: 0 }, action) => {
       counter: state.counter + 1,
     }
   }
+  // if (action.type === 'increasesby5') {
+  //   return {
+  //     counter: state.counter + 5,
+  //   }
+  // } 하드코딩 => 더미일뿐 5씩증가하는거에 대해서는 실 사용시 문제발생함
+  if (action.type === 'increase') {
+    return {
+      counter: state.counter + action.amount, // 액션으로부터 증가값을 얻는걸 예측 벨류나어마운트 추가가능
+    }
+  }
   if (action.type === 'decrement') {
     return {
       counter: state.counter - 1,
